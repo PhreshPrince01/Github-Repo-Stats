5abfa0b212e72c16b8b851a4e22b0974fbb8ef2f
Phresh Prince <princemelane14@gmail.com>
2024-09-17 21:04:08 +0200

    welcome message

--- cli.py
+++ cli.py
@@ -1,7 +1,34 @@
+from rich.text import Text
 import click
 from github_api import get_repo_data, display_stats
+from rich.console import Console
+from rich.panel import Panel
 
-@click.command()
+console = Console()
+
+
+def print_welcome_message():
+    """
+    Print a welcome message to the user.
+    """
+    text = Text("Welcome to the GitHub Repo Stats CLI Tool!", justify="center", style="bold yellow")
+    description_text = Text("Fetch and display various statistics for GitHub repository with ease.",
+                            justify="center", style="green")
+    panel = Panel.fit(text + "\n\n" + description_text,
+                      title="GitHub Stats CLI",
+                      border_style="bright_blue",
+                      padding=(1,2),)
+    console.print(panel)
+    # console.print("[bold cyan] Welcome to the GitHub Repo Stats CLI Tool![/bold cyan]")
+    # console.print("Fetch and display various statistics for GitHub repository with ease.")
+
+
+@click.group()
+def cli():
+    print_welcome_message()
+
+
+@cli.command()
 @click.option('--repo', prompt='GitHub Repository', help= 'The full name of the GitHub repository (e.g., owener/repo)')
 def get_stats(repo):
     """
@@ -11,5 +38,6 @@ def get_stats(repo):
     if data:
         display_stats(data)
 
+
 if __name__ == "__main__":
-    get_stats()
+    cli()
