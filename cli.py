d7b06990e7ba7dc9cbcf49d65e6e8108c6e7f586
Phresh Prince <princemelane14@gmail.com>
2024-09-17 21:38:29 +0200

    added confirmation prompt

--- cli.py
+++ cli.py
@@ -10,10 +10,13 @@ def cli():
 
 @cli.command()
 @click.option('--repo', prompt='GitHub Repository', help= 'The full name of the GitHub repository (e.g., owener/repo)')
-def get_stats(repo):
+@click.option('--confirm/--no-confirm', default=True, help='Require confirmation before fetching data.')
+def get_stats(repo, confirm):
     """
     CLI to get  stats for a GitHub Repository
     """
+    if confirm:
+        click.confirm(f"Are you sure you want to fetch data stats for {repo}",abort=True)
     data = get_repo_data(repo)
     if data:
         display_stats(data)
