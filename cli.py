c58c1e76027d3900b4d3cac3e9cd467bc5da4a08
Phresh Prince <princemelane14@gmail.com>
2024-09-18 23:17:03 +0200

    add pull request command

--- cli.py
+++ cli.py
@@ -65,6 +65,22 @@ def get_issues(repo, confirm):
         display_issues(data)
 
 
+@cli.command()
+@click.option('--repo', prompt='GitHub Repository', help= 'The full name of the GitHub repository (e.g., owener/repo)')
+@click.option('--confirm/--no-confirm', default=True, help='Require confirmation before fetching data.')
+def get_pull_requests(repo, confirm):
+    """
+    CLI to get commits for a GitHub Repository
+    """
+    if confirm:
+        click.confirm(f"Are you sure you want to fetch pull requests for {repo}",abort=True)
+    data = get_pull_requests_stats(repo)
+    if data:
+        display_pull_requests(data)
+
+
+
+
 
         
 
